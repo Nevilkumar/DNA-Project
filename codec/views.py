2b7c69c931487b3cdae6ea2200aa3529ae6f068a
Nevilkumar <nevil.0619@gmail.com>
2022-11-13 22:46:36 +0530

    UI Changes

--- codec/views.py
+++ codec/views.py
@@ -23,26 +23,25 @@ def encoding(bin_string):
     return encoded_string
 
 def convert_to_binary(num):
-    bnr = bin(num). replace('0b','')
-    x = bnr[::-1] #this reverses an array.
+    bnr = bin(num). replace('0b', '')
+    x = bnr[::-1]  # this reverses an array.
     while len(x) < 8:
         x += '0'
         bnr = x[::-1]
     return bnr
 
 def convert_to_decimal(num):
-    return int(num,2)
+    return int(num, 2)
 
 def create_codewords(msg, codeword_bits):
     # gen_matrix = np.random.randint(2, size=(8,codeword_bits))
     # A_matrix = np.random.randint(2, size=(8,4))
     # gen_matrix = np.concatenate((np.eye(8, dtype='int32'),A_matrix),axis=1)
-    gen_matrix = np.array([[1,1,0,0,0,0,0,1,0,0,0,0],[0,1,1,0,0,0,0,0,1,0,0,0],[0,0,1,1,0,0,0,0,0,1,0,0],[0,0,0,1,1,0,0,0,0,0,1,0],[0,0,0,0,1,1,0,0,0,0,0,1],
-      [1,0,0,0,0,1,1,0,0,0,0,0],[0,1,0,0,0,0,1,1,0,0,0,0],[0,0,1,0,0,0,0,1,1,0,0,0]])
+    gen_matrix = np.array([[1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+                           [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0]])
     msg_matrix = matrix_generator(msg, 8)
     codeword_matrix = np.dot(msg_matrix, gen_matrix)
-    return np.mod(codeword_matrix,2), gen_matrix
-
+    return np.mod(codeword_matrix, 2), gen_matrix
 
 def matrix_generator(lst_bin, n):
     msg_matrix = []
@@ -51,7 +50,7 @@ def matrix_generator(lst_bin, n):
         for ch in val:
             temp.append(int(ch))
         msg_matrix.append(temp)
-    return np.array(msg_matrix).reshape(-1,n)
+    return np.array(msg_matrix).reshape(-1, n)
 
 def cyclic_code_encoder(file, num_bits):
     with open(f'static/{file}') as f:
@@ -62,14 +61,17 @@ def cyclic_code_encoder(file, num_bits):
         for item in content:
             lst_int = [ord(ch) for ch in item]
             lst_bin = [convert_to_binary(num) for num in lst_int]
-            codewords, gen_matrix = create_codewords(lst_bin, codeword_bits=num_bits)
+            codewords, gen_matrix = create_codewords(
+                lst_bin, codeword_bits=num_bits)
             cw.extend(codewords)
             lst_encoded = [''.join(str(v) for v in list(c)) for c in codewords]
             acgt_str = [encoding(c) for c in lst_encoded]
-            lst_encoded_text += ''.join(acgt_str)    
-    
+            lst_encoded_text += ''.join(acgt_str)
+
     with open('static/output.txt', 'w+') as f:
         f.write(lst_encoded_text)
+
+
 
 # Create your views here.
 
@@ -85,6 +87,7 @@ def home(request):
             cyclic_code_encoder('input.txt', 12)
         return redirect('download')
     return render(request, 'codec/home.html')
+
 
 def download(request):
     return render(request, 'codec/download.html')
---
+++ readme.md
@@ -0,0 +1,5 @@
+# DNA Codec Techniques
+
+<img src='Readme_images/1.png' />
+&nbsp;
+<img src='Readme_images/2.png' />
